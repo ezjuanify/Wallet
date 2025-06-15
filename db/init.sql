6b14711eb0973fe2f7585beaf460888970d55ee9
Juanez <juanez@DESKTOP-OKRCR72.>
2025-06-15 13:04:03 +0800

    #1: Design DB Schema for Wallets and Transactions

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS wallets (
+    id                    SERIAL  PRIMARY KEY,
+    username              TEXT    UNIQUE         NOT NULL,
+    balance               BIGINT                 NOT NULL DEFAULT 0,
+    last_deposit_amount   BIGINT,
+    last_deposit_updated  TIMESTAMP,
+    last_withdraw_amount  BIGINT,
+    last_withdraw_updated TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id           SERIAL  PRIMARY KEY,
+    username     TEXT    UNIQUE        NOT NULL,
+    type         TEXT                  NOT NULL CHECK (type IN ('deposit', 'withdraw', 'transfer_in', 'transfer_out')),
+    amount       BIGINT                NOT NULL CHECK (amount > 0),
+    counterparty TEXT,
+    timestamp    TIMESTAMP             NOT NULL DEFAULT now(),
+    hash         TEXT                  NOT NULL
+);
